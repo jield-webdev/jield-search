729450f3e5e3869d126cbcef2aa59497f06ad87f
Johan van der Heide <johan.vanderheide@jield.nl>
2025-07-20 08:44:03 +0200

    Create shallow updates, reverse order

--- src/Command/SyncIndex.php
+++ src/Command/SyncIndex.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jield\Search\Command;
 
 use Jield\Search\Service\ConsoleService;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
